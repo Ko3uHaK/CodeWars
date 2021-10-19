830896ef0066b0687fc538d1862e3cc6ba0f293b
Ko3uHaK <den2001888@yandex.by>
2021-10-19 12:27:47 +0300

    2 new Kat's

--- solution.js
+++ solution.js
@@ -1,34 +1,48 @@
-function absentVowel(x){
-    var vowel = 'aeiou';
-    for (var i in vowel) {
-        if (x.toLowerCase().indexOf(vowel[i]) == -1) {
-          return Number(i);
-        }
-      }
-    }
-    describe("Tests", () => {
-        it("test", () => {
-      Test.assertEquals(absentVowel("John Doe hs seven red pples under his bsket"), 0);
-      Test.assertEquals(absentVowel("Bb Smith sent us six neatly arranged range bicycles"), 3);
-        });
-      });
+// function absentVowel(x){
+//     var vowel = 'aeiou';
+//     for (var i in vowel) {
+//         if (x.toLowerCase().indexOf(vowel[i]) == -1) {
+//           return Number(i);
+//         }
+//       }
+//     }
+//     describe("Tests", () => {
+//         it("test", () => {
+//       Test.assertEquals(absentVowel("John Doe hs seven red pples under his bsket"), 0);
+//       Test.assertEquals(absentVowel("Bb Smith sent us six neatly arranged range bicycles"), 3);
+//         });
+//       });
 
       //
       //
       //
       //
     //   Get the Middle Character
-    function getMiddle(s){
-        var position;
-               var length;
+    // function getMiddle(s){
+    //     var position;
+    //            var length;
        
-               if(s.length % 2 == 1) {
-                   position = s.length / 2;
-                   length = 1;
-               } else {
-                   position = s.length / 2 - 1;
-                   length = 2;
-               }
+    //            if(s.length % 2 == 1) {
+    //                position = s.length / 2;
+    //                length = 1;
+    //            } else {
+    //                position = s.length / 2 - 1;
+    //                length = 2;
+    //            }
        
-               return s.substring(position, position + length)
-       }
+    //            return s.substring(position, position + length)
+    //    }
+       //Convert a Number to a String!
+      //  function numberToString(num) {
+      //   // Return a string of the number here!
+      //  num = String(num);
+      //  return num;
+      // }
+      // numberToString(1);
+      //
+      //Convert a String to a Number!
+      // var stringToNumber = function(str){
+      //   str = parseInt(str);
+      //   return(str);
+      //   }
+        
