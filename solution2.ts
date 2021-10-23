eef46a027adfbeb81cafdc29f6cd3d994197d4ea
Ko3uHaK <den2001888@yandex.by>
2021-10-23 11:50:16 +0300

    fibonachi ts

--- solution2.ts
+++ solution2.ts
@@ -4,6 +4,27 @@
     //     return total;;
     //   }
     //Grasshopper - Variable Assignment Debug
-//     export const a : string = "dev"
-// export const b : string = "Lab"
-// export const name : string = a + b
+    //     export const a : string = "dev"
+    // export const b : string = "Lab"
+    // export const name : string = a + b
+
+    //Фибоначи
+    function* fibonacci(): IterableIterator<number> {
+        let [a, b] = [0, 1];
+      
+        while (true) {
+          yield a;
+          [a, b] = [b, a + b];
+        }
+      }
+      
+      function print(n: number) {
+        let i = 0;
+        for (const fib of fibonacci()) {
+          if (i++ === n) break;  
+          console.log(fib);
+        }  
+      }
+      
+      // Print first 10 Fibonacci numbers.
+      print(10);
